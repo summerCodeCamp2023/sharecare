45985c96a6a3a8464a00f0d565331a8036f4489a
nursetiti <nosiratalade1@gmail.com>
2023-10-24 13:16:12 +0100

    commit

---
+++ DOM1.js
@@ -0,0 +1,15 @@
+
+// document.getElementById(oy').innerHTML='ayo';
+// document.getElementsByTagName('h1')[1].innerHTML='chioma';
+// document.getElementsByClass('mujidat')[0].innerHTML='simisola';
+//document.getElementById('ai').src='ocean.jpg';
+//document.getElementById('AI').src='robot dog .jpg';
+
+
+document.getElementById('dte').innerHTML='the date and time is:'+ ""+ Date();
+document.getElementById('h4').style.color="purple";
+document.getElementById('p').style.fontFamily='impact,haettenschweiler';
+document.getElementById('p').style.fontSize='50px';
+document.getElementById('p2').style.fontWeight='bolder';
+document.getElementById('heading').style.textAlign='right';
+ 
---
+++ html/Dom.js
@@ -0,0 +1,22 @@
+// document.getElementById("Food").innerHTML = "Yam";
+// document.getElementById("Animal").innerHTML = "Goat";
+// document.getElementById("Places").innerHTML = "Lekki";
+
+
+// var names = document.getElementById("Lname");
+// document.getElementById("Fname").innerHTML= "rianot" + " " + names.innerHTML;
+
+// const names=document.getElementsByTagName("h4");
+// names[0].innerHTML='greetings'
+// names[2].innerHTML='welcome'
+
+//const names= document.getElementsByTagName("p");
+//document.getElementById("tag").innerHTML="my pet is a"  + " " + names[1].innerHTML;
+
+const data=document.getElementsByClassName("one");
+data[2] .innerHTML="slippers";
+data[1].innerHTML="ear ring";
+data[0].innerHTML="Watch";
+data[3].innerHTML="gown";
+data[4].innerHTML="trouser";
+
